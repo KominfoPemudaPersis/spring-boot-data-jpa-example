bacdced0b978f6ada257f7daedd23821686e1959
Hendi Santika <hendisantika@gmail.com>
2019-01-05 05:16:26 +0700

    #4 - Add User Entity Class

---
+++ src/main/java/com/kominfo/halaqohit/springbootdatajpaexample/entity/User.java
@@ -0,0 +1,49 @@
+package com.kominfo.halaqohit.springbootdatajpaexample.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-data-jpa-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-01-05
+ * Time: 05:14
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Entity
+@Table(name = "user")
+public class User {
+    private Long id;
+
+    private String name;
+
+    private String country;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
